da00cda09a874a1dbc21c52e451ded64db867048
Klas Kronander <klas.kronander@epfl.ch>
2014-10-31 09:48:36 +0100

    additional cleaning ignores etc

--- packages/third_party/rtk_libode/rtk_libode_scripts/cleanODE.sh
+++ packages/third_party/rtk_libode/rtk_libode_scripts/cleanODE.sh
@@ -3,4 +3,4 @@
 rtk_libode_path=$(rospack find rtk_libode)
 cd $rtk_libode_path
 # delete compiled and unpacked stuff
-rm -r bin lib ode-0.13 *.tar.bz2*
+rm -r bin lib include/* ode-0.13 *.tar.bz2* 
