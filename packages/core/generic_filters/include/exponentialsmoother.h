8aebfde9cf467ad26c06961259a066210572ec45
Klas Kronander <klas.kronander@epfl.ch>
2015-04-22 11:17:37 +0200

    modifications to generic_filters package

--- packages/core/generic_filters/include/exponentialsmoother.h
+++ packages/core/generic_filters/include/exponentialsmoother.h
@@ -6,10 +6,33 @@
 template <class T>
 class ExponentialSmoother{
 public:
-    ExponentialSmoother(const float &alpha);
-    ExponentialSmoother(const float &alpha,const T& starting_value);
-    ~ExponentialSmoother(){};
-    const T& filter(const T& inp);
+    ExponentialSmoother(const float &alpha){
+        is_initialized_ = false;
+        alpha_ = alpha;
+        //std::cout<<old_average_<<std::endl;
+    }
+    ExponentialSmoother(const float &alpha,const T& starting_value){
+        new_output_ = starting_value;
+        is_initialized_ = true;
+        alpha_ = alpha;
+        //std::cout<<old_average_<<std::endl;
+    }
+    ~ExponentialSmoother(){}
+    void set_initialized(bool init){
+        is_initialized_ = init;
+    }
+
+    const T& filter(const T& inp){
+        if(!is_initialized_){
+            new_output_ = inp;
+            is_initialized_ = true;
+        }
+        //new_output_.Print();
+        //exit(1);
+        new_output_ *= (1.0-alpha_);
+        new_output_ += (inp*alpha_);
+        return new_output_;
+    }
 
 private:
     T new_output_;
@@ -17,33 +40,33 @@ private:
     bool is_initialized_;
 };
 
-template<class T>
-ExponentialSmoother<T>::ExponentialSmoother(const float& alpha){
-    is_initialized_ = false;
-    alpha_ = alpha;
-    //std::cout<<old_average_<<std::endl;
-};
+//template<class T>
+//ExponentialSmoother<T>::ExponentialSmoother(const float& alpha){
+//    is_initialized_ = false;
+//    alpha_ = alpha;
+//    //std::cout<<old_average_<<std::endl;
+//};
 
-template<class T>
-ExponentialSmoother<T>::ExponentialSmoother(const float& alpha,const T& starting_value){
-    new_output_ = starting_value;
-    is_initialized_ = true;
-    alpha_ = alpha;
-    //std::cout<<old_average_<<std::endl;
-};
+//template<class T>
+//ExponentialSmoother<T>::ExponentialSmoother(const float& alpha,const T& starting_value){
+//    new_output_ = starting_value;
+//    is_initialized_ = true;
+//    alpha_ = alpha;
+//    //std::cout<<old_average_<<std::endl;
+//};
 
-template<class T>
-const T& ExponentialSmoother<T>::ExponentialSmoother::filter(const T& inp){
-    if(!is_initialized_){
-        new_output_ = inp;
-        is_initialized_ = true;
-    }
-    //new_output_.Print();
-    //exit(1);
-    new_output_ *= (1.0-alpha_);
-    new_output_ += (inp*alpha_);
-    return new_output_;
-}
+//template<class T>
+//const T& ExponentialSmoother<T>::ExponentialSmoother::filter(const T& inp){
+//    if(!is_initialized_){
+//        new_output_ = inp;
+//        is_initialized_ = true;
+//    }
+//    //new_output_.Print();
+//    //exit(1);
+//    new_output_ *= (1.0-alpha_);
+//    new_output_ += (inp*alpha_);
+//    return new_output_;
+//}
 
 template<class T>
 class ApproximateMovingAverage : public ExponentialSmoother<T>{
